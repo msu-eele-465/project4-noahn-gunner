03113ee1671e7dbfa5079ef9ff4ac9245d13bd01
Noah Netz <noahjnetz@gmail.com>
2025-03-20 22:04:39 -0600

    Added psudocode and did some testing of the GPIO ports

--- i2c-led-bar/app/main.c
+++ i2c-led-bar/app/main.c
@@ -1,22 +1,42 @@
-#include <msp430fr2310.h>
-#include <stdbool.h>
+#include <msp430.h>
 
-int main(void)
-{
-    // Stop watchdog timer
-    WDTCTL = WDTPW | WDTHOLD;
+// Define LED Pins for MSP430FR2310
+#define LED1 BIT0  // P2.0
+#define LED2 BIT7  // P2.7
+#define LED3 BIT6  // P2.6
+#define LED4 BIT7  // P1.7
+#define LED5 BIT6  // P1.6
+#define LED6 BIT5  // P1.5
+#define LED7 BIT4  // P1.4
+#define LED8 BIT1  // P1.1
 
-    P1OUT &= ~BIT0;
-    P1DIR |= BIT0;
+void initLEDs(void) {
+    WDTCTL = WDTPW | WDTHOLD;  // Stop watchdog timer
 
-    // Disable low-power mode / GPIO high-impedance
-    PM5CTL0 &= ~LOCKLPM5;
+    PM5CTL0 &= ~LOCKLPM5;  // Ensure GPIO is enabled
 
-    while (true)
-    {
-        P1OUT ^= BIT0;
+    // Force P2.6 & P2.7 into GPIO mode
+    P2SEL0 &= ~(BIT6 | BIT7);
+    P2SEL1 &= ~(BIT6 | BIT7);
 
-        // Delay for 100000*(1/MCLK)=0.1s
-        __delay_cycles(100000);
-    }
+    // Set all LED pins as outputs
+    P1DIR |= (BIT7 | LED5 | LED6 | LED7 | LED8);  
+    // P2DIR |= (LED1 | LED2 | LED3);                
+
+    // // Turn all LEDs ON for testing
+    P1OUT |= BIT7 | LED5 | LED6 | LED7 | LED8;
+    // P2OUT = LED1 | LED2 | LED3;
+
+    // P2DIR |= BIT7;
+    // P2OUT |= BIT7;
+
+    // P2DIR |= BIT6;
+    // P2OUT |= BIT6;
+
+    //PM5CTL0 &= ~LOCKLPM5;  // Ensure GPIO is enabled
+}
+
+int main(void) {
+    initLEDs();  // Initialize LED GPIOs
+    while (1);   // Stay in an infinite loop
 }
